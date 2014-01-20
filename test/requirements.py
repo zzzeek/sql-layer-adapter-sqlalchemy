ee35402b58b4dffac54980e78830afeacbc7bcb3
Mike Bayer <mike_mp@zzzcomputing.com>
2014-01-20 16:24:44 -0500

    - unique constraint reflection not implemented

--- test/requirements.py
+++ test/requirements.py
@@ -16,6 +16,10 @@ class Requirements(SuiteRequirements):
         return exclusions.closed()
 
     @property
+    def unique_constraint_reflection(self):
+        return exclusions.closed()
+
+    @property
     def view_reflection(self):
         return exclusions.closed()
 
